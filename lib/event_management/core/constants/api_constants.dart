2d96b3775be2f22337dd1fa7bd06b0f15fccedb9
sumanth-hm-19 <sumanthhakkare894@gmail.com>
2026-02-14 17:09:58 +0530

    base_url_change

--- lib/event_management/core/constants/api_constants.dart
+++ lib/event_management/core/constants/api_constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
   static String get baseUrl =>
-      dotenv.env['API_BASE_URL'] ?? 'http://localhost:8000/api';
+      dotenv.env['BASE_URL'] ?? 'http://localhost:8000/api';
 
   // ── Event endpoints ──
   static String get eventsUrl => '$baseUrl/events/';
